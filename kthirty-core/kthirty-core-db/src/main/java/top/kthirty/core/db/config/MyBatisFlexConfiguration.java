ae6388d253521a8df9741d7cd723d60a8d923c7e
KTIHRTY <15132028484@163.com>
2024-04-11 13:28:10 +0800

    fix(db): listener 失效处理

--- kthirty-core/kthirty-core-db/src/main/java/top/kthirty/core/db/config/MyBatisFlexConfiguration.java
+++ kthirty-core/kthirty-core-db/src/main/java/top/kthirty/core/db/config/MyBatisFlexConfiguration.java
@@ -9,6 +9,8 @@ import com.mybatisflex.spring.boot.MybatisFlexAutoConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Configuration;
+import top.kthirty.core.db.base.entity.BaseEntity;
+import top.kthirty.core.db.base.entity.DataEntity;
 import top.kthirty.core.db.listener.OperatingListener;
 @Slf4j
 @Configuration
@@ -25,8 +27,8 @@ public class MyBatisFlexConfiguration implements MyBatisFlexCustomizer {
         // 是否打印Banner
         flexGlobalConfig.setPrintBanner(false);
         // 插入与更新监听
-        flexGlobalConfig.registerInsertListener(new OperatingListener());
-        flexGlobalConfig.registerUpdateListener(new OperatingListener());
+        flexGlobalConfig.registerInsertListener(new OperatingListener(), BaseEntity.class);
+        flexGlobalConfig.registerUpdateListener(new OperatingListener(), BaseEntity.class);
         // Sql审计功能
         AuditManager.setAuditEnable(true);
         AuditManager.setMessageCollector(auditMessage -> log.debug("{},{}ms", auditMessage.getFullSql(), auditMessage.getElapsedTime()));
