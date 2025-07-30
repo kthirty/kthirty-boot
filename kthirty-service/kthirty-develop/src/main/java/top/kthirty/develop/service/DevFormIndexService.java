bd615bfc3b20e39feb5e23326de59d91cc2f441a
KTIHRTY <15132028484@163.com>
2025-07-30 16:16:00 +0800

    feat(controller): 删除 DevFormItemController，新增 DevFormIndex 相关的映射和服务层实现

---
+++ kthirty-service/kthirty-develop/src/main/java/top/kthirty/develop/service/DevFormIndexService.java
@@ -0,0 +1,14 @@
+package top.kthirty.develop.service;
+
+import top.kthirty.core.db.base.service.BaseService;
+import top.kthirty.develop.entity.DevFormItem;
+
+/**
+ *  服务层。
+ *
+ * @author KTHIRTY
+ * @since 2024-07-02
+ */
+public interface DevFormItemService extends BaseService<DevFormItem> {
+
+}
---
+++ kthirty-service/kthirty-develop/src/main/java/top/kthirty/develop/service/impl/DevFormIndexServiceImpl.java
@@ -0,0 +1,18 @@
+package top.kthirty.develop.service.impl;
+
+import com.mybatisflex.spring.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import top.kthirty.develop.entity.DevFormItem;
+import top.kthirty.develop.mapper.DevFormItemMapper;
+import top.kthirty.develop.service.DevFormItemService;
+
+/**
+ *  服务层实现。
+ *
+ * @author KTHIRTY
+ * @since 2024-07-02
+ */
+@Service
+public class DevFormItemServiceImpl extends ServiceImpl<DevFormItemMapper, DevFormItem> implements DevFormItemService {
+
+}
