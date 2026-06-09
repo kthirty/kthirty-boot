bdcfd4862b663badc4f47d3e32494e496957d8e3
KTIHRTY <5264thirty@gmail.com>
2026-06-09 16:25:42 +0800

    fix(*): 升级后出现的问题修改

--- kthirty-extra/kthirty-report/src/main/java/top/kthirty/extra/report/config/ReportController.java
+++ kthirty-extra/kthirty-report/src/main/java/top/kthirty/extra/report/config/ReportController.java
@@ -4,9 +4,11 @@ import com.bstek.ureport.provider.report.ReportFile;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import top.kthirty.core.web.base.BaseController;
 import top.kthirty.extra.report.config.provider.DbReportProvider;
 
@@ -21,8 +23,10 @@ import java.util.List;
 @RequestMapping("report")
 @Tag(name = "报表接口")
 @AllArgsConstructor
+@RestController
 public class ReportController extends BaseController {
-    private final DbReportProvider dbReportProvider;
+    @Autowired(required = false)
+    private DbReportProvider dbReportProvider;
 
     @GetMapping("list")
     @Operation(summary = "查询报表列表", description = "查询报表列表")
